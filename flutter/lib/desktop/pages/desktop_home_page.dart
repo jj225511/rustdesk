c267acd01a5e57fb6e70110e2b3fce431c9257bd
fufesou <linlong1266@gmail.com>
2025-11-24 23:20:18 +0800

    test
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -448,7 +448,7 @@ class _DesktopHomePageState extends State<DesktopHomePage>
       }
       return buildInstallCard(
           "Status",
-          "${translate("new-version-of-{${bind.mainGetAppNameSync()}}-tip")} (1.4.6).",
+          "${translate("new-version-of-{${bind.mainGetAppNameSync()}}-tip")} (1.4.7).",
           btnText,
           onPressed,
           closeButton: true);
